c5d2a64bd3b4708c85321b9f484cac7c6044ebbb
Olanrewaju Abidogun <olanrewaju.abidogun@gmail.com>
2021-11-01 16:29:00 +0100

    Test: setup testing environment

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(
+        [
+            "success" => true,
+            "message" => "Hey, you shouldn't be here :)"
+        ],
+        Response::HTTP_OK
+    );
 });

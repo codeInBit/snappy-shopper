4403fc125525dd6ea59f713bae1f16c51114949c
Olanrewaju Abidogun <olanrewaju.abidogun@gmail.com>
2021-12-08 16:00:23 +0100

    Feat: Create model, migration and resource for agent. - Create seeder to populate agent records in DB

---
+++ app/Models/Agent.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Agent extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name', 'last_name', 'email', 'phone'
+    ];
+
+    /**
+     * Get the agent's full name.
+     *
+     * @return string
+     */
+    public function getFullNameAttribute(): string
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
+
+    /**
+     * The properties that belong to the agent.
+     */
+    public function properties()
+    {
+        return $this->belongsToMany('App\Models\Property');
+    }
+}
